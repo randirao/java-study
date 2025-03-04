46bddbd47b449af2fd726c65c50e30bd657f3e5b
Han2 <randirao4245@gmail.com>
2025-03-04 14:40:18 +0900

    Add Ref

--- src/ref/VarChange2.java
+++ src/ref/VarChange2.java
@@ -8,19 +8,19 @@ public class VarChange2 {
 
         System.out.println("dataA 참조값 = " + dataA);
         System.out.println("dataB 참조값 = " + dataB);
-        System.out.println("dataA.value = " + dataA.value);
-        System.out.println("dataB.value = " + dataB.value);
+        System.out.println("dataA.value = " + dataA.value); //10
+        System.out.println("dataB.value = " + dataB.value); //10
 
         //dataA 변경
         dataA.value = 20;
         System.out.println("변경 dataA.value = 20");
-        System.out.println("dataA.value = " + dataA.value);
-        System.out.println("dataB.value = " + dataB.value);
+        System.out.println("dataA.value = " + dataA.value); //20
+        System.out.println("dataB.value = " + dataB.value); //20
 
         //dataB 변경
         dataB.value = 30;
         System.out.println("변경 dataB.value = 30");
-        System.out.println("dataA.value = " + dataA.value);
-        System.out.println("dataB.value = " + dataB.value);
+        System.out.println("dataA.value = " + dataA.value); //30
+        System.out.println("dataB.value = " + dataB.value); //30
     }
 }
---
+++ src/ref/initMain.java
@@ -0,0 +1,10 @@
+package ref;
+
+public class initMain {
+    public static void main(String[] args) {
+        initData data = new initData();
+
+        System.out.println("value1 = " + data.value1);
+        System.out.println("value2 = " + data.value2);
+    }
+}
